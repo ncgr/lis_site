efa857e806022a09532eadce34fee1b4c0812702
Ken Seal <kas@ncgr.org>
2010-07-20 16:24:10 -0600

    Revised custom error messages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,16 @@
 class ApplicationController < ActionController::Base
     helper :all 
     protect_from_forgery
+    rescue_from ActionView::MissingTemplate, :with => :render_missing_template
 
     protected
     def render_optional_error_file(status_code)
         status = interpret_status(status_code)
         render :template => "/errors/#{status[0,3]}.html.erb", :status => status, :layout => 'default.html.erb'
+    end
+    
+    def render_missing_template
+        render :template => "/errors/404.html.erb", :layout => 'default.html.erb'
     end
 
 end
