615e7b3887d987f7f892bde5f3b256b6aa2b8765
Ken Seal <kas@ncgr.org>
2010-07-23 08:55:45 -0600

    Protected application controller methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < ActionController::Base
     protect_from_forgery
     rescue_from ActionView::MissingTemplate, :with => :render_missing_template
 
-    protected
     ##
     # Override render_optional_error_file to display our custom error pages using
     # our default layout.
     #
     # @param Int status_code
     #
+    protected
     def render_optional_error_file(status_code)
         status = interpret_status(status_code)
         render :template => "/errors/#{status[0,3]}.html.erb", :status => status, :layout => 'default.html.erb'
@@ -23,6 +23,7 @@ class ApplicationController < ActionController::Base
     # I am only doing this to accomidate our static pages in app/pages/. See
     # routes.rb and app/controllers/pages_controller.rb for more information. 
     #
+    protected
     def render_missing_template
         render :template => "/errors/404.html.erb", :layout => 'default.html.erb'
     end
