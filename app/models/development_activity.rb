56aacf81265ef898abc32f24f9372b23cd8295f5
Ken Seal <kas@ncgr.org>
2010-12-16 16:55:26 -0700

    Added Development Activity, style and layout changes.

--- app/models/development_activity.rb
+++ app/models/development_activity.rb
@@ -1,18 +1,18 @@
 ##
-# NewsArticle Model
+# DevelopmentActivity Model
 # 
 # Author: Ken Seal - NCGR
 #
 
-class NewsArticle < ActiveRecord::Base
+class DevelopmentActivity < ActiveRecord::Base
     
-    def self.get_recent_news(limit = nil)
+    def self.get_recent_activities(limit = nil)
         limit ||= 1
-        return find(:all, :order => "timestamp DESC", :limit => limit)
+        return find(:all, :order => "id DESC", :limit => limit)
     end
     
     def self.paginate_by_params(page, order = nil)
-        order ||= "timestamp DESC"
+        order ||= "id DESC"
         return paginate(:all, :per_page => 20, :page => page, :order => order)
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "home", :action => "index"
   
-  map.resources :news_articles
+  map.resources :news_articles, :development_activities
   
   # Only serves static pages.
   map.page ":page", :controller => "pages", :action => "show", :page => /[a-z_]+/
