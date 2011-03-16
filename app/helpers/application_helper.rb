12a7841172cb295364fbf5f2d15b1885d0626f74
Ken Seal <kas@ncgr.org>
2011-03-16 17:08:34 -0600

    Added Meetings and changed styles and layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,26 @@ module ApplicationHelper
         return timestamp.strftime("%a, %b %d %Y, %H:%M")
     end
     
+    ##
+    # Date helper.
+    #
+    def display_date(date)
+        return date.strftime("%b %d, %Y")
+    end
+    
+    ##
+    # Start end date helper.
+    #
+    def format_start_end_date(s_date, e_date = nil)
+        if e_date.nil?
+            return dislpay_date(s_date)
+        end
+        if (s_date.mon == e_date.mon) && (s_date.year == e_date.year)
+            date = s_date.strftime("%b %d-").to_s + e_date.strftime("%d, %Y").to_s
+        elsif s_date.year == e_date.year
+            date = s_date.strftime("%b %d - ").to_s + e_date.strftime("%b %d, %Y").to_s
+        end
+        return date
+    end
+    
 end
