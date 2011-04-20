e72e286b0eed7b82f639921599732495952eac51
Ken Seal <kas@ncgr.org>
2011-04-20 14:11:39 -0600

    Updated SQL and added release script.

---
+++ release-tar.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# Creates a tarball ready to deploy.
+# 
+tar czf lis_site-release.tar.gz \
+    lis_site/app \
+    lis_site/config \
+    lis_site/config.ru \
+    lis_site/db \
+    lis_site/doc \
+    lis_site/Gemfile \
+    lis_site/Gemfile.lock \
+    lis_site/lib \
+    lis_site/public \
+    lis_site/Rakefile \
+    lis_site/README \
+    lis_site/script \
+    lis_site/test \
+    lis_site/vendor 
+
+# omitted files:
+# log 
+# startServer
+# tmp
+
