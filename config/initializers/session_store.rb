2e2ecda6a0d69d8b7bdd6107f4bc66c30b39fc47
Ken Seal <kas@ncgr.org>
2011-05-27 10:23:03 -0600

    Fixed HTML bug on home page. Removed deprecated method.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-LisSite::Application.config.session_store :active_record_store
-
-ActionController::Base.session = {
+LisSite::Application.config.session_store :active_record_store,
   :key => '_lis_site_session',
   :expire_after => 2.days
-}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
