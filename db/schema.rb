969b7fe7cda08bc6a344e9a62e834f9fcb51b2dd
Ken Seal <kas@ncgr.org>
2011-05-27 13:01:31 -0600

    Updated SQL for production.

--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,6 @@ ActiveRecord::Schema.define(:version => 0) do
     t.string   "file_name"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.integer  "legume_id",           :null => false
   end
 
   create_table "meetings", :force => true do |t|
@@ -110,12 +109,21 @@ ActiveRecord::Schema.define(:version => 0) do
     t.string   "file_name"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.integer  "legume_id",           :null => false
   end
 
   create_table "roles", :force => true do |t|
     t.string "name"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "user_roles", :force => true do |t|
     t.integer "user_id", :null => false
