748d63c4a9f8badc9c8ed57b6ade509b97086041
Ken Seal <kas@ncgr.org>
2011-06-13 16:25:31 -0600

    Added CKFinder. Added Image tag to CKEditor (testing phase only). Added medsa to phylogenetic tree (not active).

---
+++ public/javascripts/ckfinder/config.js
@@ -0,0 +1,10 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckfinder.com/license
+*/
+
+CKFinder.customConfig = function( config )
+{
+	config.skin = 'v1';
+	config.language = 'en';
+};
