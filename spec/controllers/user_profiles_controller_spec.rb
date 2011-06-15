c1c53844e6e324c21cece65361a96b625c938b87
Ken Seal <kas@ncgr.org>
2011-06-15 16:06:35 -0600

    Added controller tests to make sure signed in users can see their profile, and to ensure the ckfinder_role cookie is set.

--- spec/controllers/user_profiles_controller_spec.rb
+++ spec/controllers/user_profiles_controller_spec.rb
@@ -6,6 +6,14 @@ describe UserProfilesController do
     @mock_user ||= mock_model(User, stubs).as_null_object
   end
 
+  #
+  # Descode signed cookies created after a user successfully logs in.
+  # app/controllers/application_controller.rb
+  #
+  def decode_signed_cookie(cookie)
+    Base64.decode64(cookie.split('--').first).gsub(/[^a-z]/, '')
+  end
+
   before(:all) do
     create_roles
   end
@@ -13,6 +21,14 @@ describe UserProfilesController do
   describe "access restricted without logging in" do
     it "index should redirect" do
       get :index
+      response.should be_redirect
+    end
+    it "show/:id should redirect" do
+      get :show, :id => "5"
+      response.should be_redirect
+    end
+    it "manage_roles/:id should redirect" do
+      get :manage_roles, :id => "8"
       response.should be_redirect
     end
     it "destroy/:id should redirect" do
@@ -50,6 +66,62 @@ describe UserProfilesController do
     it "should redirect" do
       get :index
       response.should be_redirect
+    end
+  end
+
+  describe "GET show while logged in as superuser" do
+    login_superuser
+    before(:each) do
+      @user = User.order("created_at DESC").first 
+      User.should_receive(:find).with(@user.id).and_return(@user)
+    end
+    it "should get show and set ckfinder_role cookie" do
+      request.cookies['ckfinder_role'] = nil
+      get :show, :id => @user.id
+      response.should be_success
+      decode_signed_cookie(response.cookies['ckfinder_role']).should == "superuser"
+    end
+  end
+
+  describe "GET show while logged in as admin" do
+    login_admin
+    before(:each) do
+      @user = User.order("created_at DESC").first
+      User.should_receive(:find).with(@user.id).and_return(@user)
+    end
+    it "should get show and set ckfinder_role cookie" do
+      request.cookies['ckfinder_role'] = nil
+      get :show, :id => @user.id
+      response.should be_success
+      decode_signed_cookie(response.cookies['ckfinder_role']).should == "admin"
+    end
+  end
+
+  describe "GET show while logged in as editor" do
+    login_editor
+    before(:each) do
+      @user = User.order("created_at DESC").first
+      User.should_receive(:find).with(@user.id).and_return(@user)
+    end
+    it "should get show and set ckfinder_role cookie" do
+      request.cookies['ckfinder_role'] = nil
+      get :show, :id => @user.id
+      response.should be_success
+      decode_signed_cookie(response.cookies['ckfinder_role']).should == "editor"
+    end
+  end
+
+  describe "GET show while logged in as system_user" do
+    login_system_user
+    before(:each) do
+      @user = User.order("created_at DESC").first
+      User.should_receive(:find).with(@user.id).and_return(@user)
+    end
+    it "should get show and set ckfinder_role cookie" do
+      request.cookies['ckfinder_role'] = nil
+      get :show, :id => @user.id
+      response.should be_success
+      decode_signed_cookie(response.cookies['ckfinder_role']).should == "systemuser"
     end
   end
 
