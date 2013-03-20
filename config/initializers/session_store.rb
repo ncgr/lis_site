73e9c3f62495c2d3eb879286c50868a5a32356d8
Ken Seal <kas@ncgr.org>
2013-03-20 10:35:55 -0600

    Set session store to production env.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,8 @@
 
 LisSite::Application.config.session_store :active_record_store,
   :key    => '_lis_site_session',
-  :domain => :all
+  :domain => :all,
+  :secure => Rails.env.production?
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
